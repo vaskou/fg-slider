ef1064c05c084d477670edaac2c59b4eccc64460
Vasilis Koutsopoulos <vaskou@hotmail.com>
2021-02-09 19:39:22 +0200

    -WIP

--- includes/class-fg-slider.php
+++ includes/class-fg-slider.php
@@ -6,6 +6,14 @@ class FG_Slider {
 
 	private static $instance = null;
 
+	public static function instance() {
+		if ( self::$instance == null ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
 	/**
 	 * FG_Slider constructor.
 	 */
@@ -13,17 +21,11 @@ class FG_Slider {
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_styles' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
 
+		add_action( 'plugins_loaded', array( $this, 'on_plugins_loaded' ) );
+
 		FG_Slider_Dependencies::instance();
 		FG_Slider_Post_Type::instance();
 		FG_Slider_Shortcodes::instance();
-	}
-
-	public static function instance() {
-		if ( self::$instance == null ) {
-			self::$instance = new self();
-		}
-
-		return self::$instance;
 	}
 
 	public function enqueue_admin_styles() {
@@ -36,6 +38,10 @@ class FG_Slider {
 
 		$version = $this->_get_file_version( FG_SLIDER_PLUGIN_DIR_PATH . 'assets/js/admin_scripts.js' );
 		wp_enqueue_script( 'fg-slider-css', FG_SLIDER_PLUGIN_URL . '/assets/js/admin_scripts.js', array( 'jquery' ), $version, true );
+	}
+
+	public function on_plugins_loaded() {
+		load_plugin_textdomain( 'fg-slider', false, FG_SLIDER_PLUGIN_DIR_NAME . '/languages/' );
 	}
 
 	private function _get_file_version( $filename ) {
