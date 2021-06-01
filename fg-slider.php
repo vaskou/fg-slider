c135da8d6489e7909fb854f0a191ffa58400a16e
Vasilis Koutsopoulos <vaskou@hotmail.com>
2021-06-01 19:11:18 +0300

    -WIP

--- fg-slider.php
+++ fg-slider.php
@@ -4,7 +4,7 @@
  * @wordpress-plugin
  * Plugin Name:       FremeditiGuitars - Slider
  * Description:       FremeditiGuitars - Slider - Slideshow
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Vasilis Koutsopoulos
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
@@ -14,7 +14,7 @@
 
 defined( 'ABSPATH' ) or die();
 
-define( 'FG_SLIDER_VERSION', '1.0.1' );
+define( 'FG_SLIDER_VERSION', '1.0.2' );
 define( 'FG_SLIDER_PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
 define( 'FG_SLIDER_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 define( 'FG_SLIDER_PLUGIN_DIR_NAME', basename( FG_SLIDER_PLUGIN_DIR_PATH ) );
